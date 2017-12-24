5a9e9aff0726308dcfa8dd3b17570b383336919c
raoxiaohuihui <raohuisagua@163.com>
2017-12-24 16:40:58 +0800

    update copy_control.cpp

--- stl/copy_control.cpp
+++ stl/copy_control.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <set>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -40,31 +41,136 @@ HasPtr & HasPtr::operator=(const HasPtr &rhp){
     return *this;
 }
 
+class Message;
 class Folder{
+    public:
+        void addMsg(Message *m);
+        void remMeg(Message *m);
+        virtual ~Folder();
+    private:
+        set<Message*> messages;
+        void remove_all_message();
 
 };
+
+void Folder::addMsg(Message *m){
+    messages.insert(m);
+}
+
+void Folder::remMeg(Message *m){
+    messages.erase(m);
+}
+
+void Folder::remove_all_message(){
+    for(auto m:messages){
+        messages.erase(m);
+    }
+}
+
+Folder::~Folder(){
+    //必须释放掉存有的message指针，不然可能存储空的message指针
+    remove_all_message();
+    cout << "~Folder" << endl;
+}
 
 class Message{
     friend class Folder;
+    friend void swap(Message &lm,Message &rm);
     public:
-        explicit Message(const string &s = string()):content(s){}
-        Message(const Message &m);
-        Message &operator= (const Message &m);
-        void save(Folder&f);
-        void remove(Folder&f);
+    explicit Message(const string &s = string()):content(s){}
+    Message(const Message &m);
+    Message &operator= (const Message &m);
+    void save(Folder&f);
+    void remove(Folder&f);
+    virtual ~Message();
+    void print(){
+        cout << "content:" << content << endl;
+        for_each(folders.cbegin(),folders.cend(),[](Folder *f){cout << f << endl;});
+    }
     private:
-        string content;
-        set<Folder*> folders;
-        void add_to_folders(const Message & m);
-        void remove_from_folders();
+    string content;
+    set<Folder*> folders;
+    void add_to_folders(const Message & m);
+    void remove_from_folders();
 };
 
+void swap(Message &lm,Message &rm){
+    using std::swap;
+
+    for(auto f:lm.folders){
+        f->remMeg(&lm);
+    }
+    for(auto f:rm.folders){
+        f->remMeg(&rm);
+    }
+    swap(lm.content,rm.content);
+    swap(lm.folders,rm.folders);
+
+    for(auto f:lm.folders){
+        f->addMsg(&lm);
+    }
+    for(auto f:rm.folders){
+        f->remMeg(&rm);
+    }
+
+
+}
+void Message::add_to_folders(const Message &m){
+    for(auto f:m.folders){
+        f->addMsg(this);
+    }
+}
+
+void Message::remove_from_folders(){
+    for(auto f: folders){
+        f->remMeg(this);
+    }
+}
+
+Message::Message(const Message &m):content(m.content),folders(m.folders){
+    add_to_folders(*this);
+}
+
+Message::~Message(){
+    cout << "~Message" << endl;
+    remove_from_folders();
+}
+
+Message & Message::operator=(const Message &m){
+    remove_from_folders();
+    content = m.content;
+    folders = m.folders;
+    add_to_folders(*this);
+    return *this;
+
+}
+
+
+void Message::save(Folder &f){
+    folders.insert(&f);
+    f.addMsg(this);
+
+}
+
+void Message::remove(Folder &f){
+    folders.erase(&f);
+    f.remMeg(this);
+}
+
 int main(){
-    HasPtr hp("raohui");
-    hp.print();
-    HasPtr hp1 = hp;
-    hp.print();
-    hp1.print();
+    //HasPtr hp("raohui");
+    //hp.print();
+    //HasPtr hp1 = hp;
+    //hp.print();
+    //hp1.print();
+
+    Message test("raohui");
+    Folder f;
+    test.save(f);
+    test.print();
+    Message test1 = test;
+    test1.print();
+
     return 0;
 
 }
