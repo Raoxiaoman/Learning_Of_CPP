20ca89f3fd6dcc49fc2af2d84d7375c97c5454cb
raoxiaohuihui <raohuisagua@163.com>
2018-02-26 16:09:19 +0800

    add virtual_table.cpp

--- stl/virtual_table.cpp
+++ stl/virtual_table.cpp
@@ -42,15 +42,18 @@ int main(){
     Fun pfun = NULL;
 
     cout << "基类" << endl;
-    cout << "虚函数表地址:" << (int*)(&b) << endl;
-    cout << "虚函数表--第一个虚函数的地址:" << (int*)(*(int*)(&b)) << endl;
+    cout << "虚函数表指针的地址:" << (int*)(&b) << endl;
+    cout << "虚函数表的地址--第一个虚函数的地址:" << (int*)(*(int*)(&b)) << endl;
     pfun = (Fun)*((int*)*(int*)(&b));
     pfun();
 
     derive1 d;
+    derive1 dd;
     cout << "子类1" << endl;
-    cout << "虚函数表地址:" << (int*)(&d) << endl;
-    cout << "虚函数表--第一个虚函数的地址:" << (int*)(*(int*)(&d)) << endl;
+    cout << "虚函数表指针的地址:" << (int*)(&d) << endl;
+    cout << "虚函数表的地址--第一个虚函数的地址:" << (int*)(*(int*)(&d)) << endl;
+    cout << "虚函数表指针的地址:" << (int*)(&dd) << endl;
+    cout << "虚函数表的地址--第一个虚函数的地址:" << (int*)(*(int*)(&dd)) << endl;
     long * ifun = (long*)*(int*)(&d);
     //pfun = (Fun)ifun[0];
     pfun = (Fun)*(ifun+0);
@@ -62,8 +65,8 @@ int main(){
     
     derive2 d2;
     cout << "子类2" << endl;
-    cout << "虚函数表地址:" << (int*)(&d2) << endl;
-    cout << "虚函数表--第一个虚函数的地址:" << (int*)(*(int*)(&d2)) << endl;
+    cout << "虚函数表指针的地址:" << (int*)(&d2) << endl;
+    cout << "虚函数表的地址--第一个虚函数的地址:" << (int*)(*(int*)(&d2)) << endl;
     ifun = (long*)*(int*)(&d2);
     pfun = (Fun)ifun[0];
     pfun();
